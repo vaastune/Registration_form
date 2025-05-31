ab47e72a148b9f54615691759170f3e3d14c9edb
Kabwe Ruth <kabwemarbles@gmail.com>
2025-05-31 21:57:51 +0700

    Registration_form

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.getElementById("registrationForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // prevent actual submission
+
+  const password = document.getElementById("password").value;
+  const confirm = document.getElementById("confirmPassword").value;
+
+  if (password !== confirm) {
+    alert("Passwords do not match!");
+  } else {
+    alert("Registration successful!");
+
+  }
+});
